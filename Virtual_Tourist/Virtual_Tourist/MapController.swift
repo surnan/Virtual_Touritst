52defbc3ec8c20cfb6244f808ca65c1fe78e260a
Suresh N <forsuresh@yandex.com>
2019-02-26 20:43:44 -0500

    successfully runs without storyboard

--- Virtual_Tourist/Virtual_Tourist/MapController.swift
+++ Virtual_Tourist/Virtual_Tourist/MapController.swift
@@ -8,11 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MapController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        view.backgroundColor = UIColor.yellow
+        
+        
     }
 
 
