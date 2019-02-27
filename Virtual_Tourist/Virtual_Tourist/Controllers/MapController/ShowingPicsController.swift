345228843ba2a463a30a0fa2aff7139ab2ae3b38
Suresh N <forsuresh@yandex.com>
2019-02-26 20:59:11 -0500

    MapController Navigation Page initialized with RightBarButton setup ShowingPicsController created
    Successfully going back and forth between both

---
+++ Virtual_Tourist/Virtual_Tourist/Controllers/MapController/ShowingPicsController.swift
@@ -0,0 +1,23 @@
+//
+//  ShowingPicsController.swift
+//  Virtual_Tourist
+//
+//  Created by admin on 2/26/19.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import UIKit
+
+
+class ShowingPicsController: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        view.backgroundColor = UIColor.red
+    }
+    
+    
+    
+    
+}
