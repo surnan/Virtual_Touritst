5b7756bdf1b961066357b0e7c2f203a7fb4b9c3c
Suresh N <forsuresh@yandex.com>
2019-03-06 13:59:07 -0500

    successfully opening Images VC

--- Virtual_Tourist/Virtual_Tourist/Controllers/MapController/First Controller/MapController+MapKit.swift
+++ Virtual_Tourist/Virtual_Tourist/Controllers/MapController/First Controller/MapController+MapKit.swift
@@ -86,7 +86,7 @@ extension MapController: MKMapViewDelegate {
                     })
                 }
             })
-            self.navigationController?.pushViewController(FlickrCollectionController(), animated: true)
+            self.navigationController?.pushViewController(FlickrCollectionController(collectionViewLayout: UICollectionViewFlowLayout()), animated: true)
         })
     }
 }
