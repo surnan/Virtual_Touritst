b6c1b6b6721821a26d80b666530bf136ece35c86
Suresh N <forsuresh@yandex.com>
2019-03-13 15:27:53 -0400

    moved networking code into each view controller.  Will try to cancel it within each class.  Instead of globally.

--- Virtual_Tourist/Virtual_Tourist/Controllers/Second Controller/CollectionMapViewController+Handlers.swift
+++ Virtual_Tourist/Virtual_Tourist/Controllers/Second Controller/CollectionMapViewController+Handlers.swift
@@ -49,4 +49,43 @@ extension CollectionMapViewController {
         myMapView.centerCoordinate = firstAnnotation.coordinate
     }
     
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    func connectPhotoAndPin(dataController: DataController, pin: Pin, data: Data, urlString: String){
+        let tempPhoto = Photo(context: dataController.viewContext)
+        tempPhoto.imageData = data
+        tempPhoto.urlString = urlString
+        tempPhoto.index = Int32(999) //Random value for init
+        tempPhoto.pin = pin
+        let testImage = UIImage(data: tempPhoto.imageData!)
+        try? dataController.viewContext.save()
+    }
+    
+    func downloadNearbyPhotosToPin(dataController: DataController, currentPin: Pin, fetchCount: Int) {
+        //TODO: User should get an indicator that cell count = zero because download incoming?  Loading cells don't show here
+        FlickrClient.searchNearbyPhotoData(currentPin: currentPin, fetchCount: fetchCount) { (urls, error) in
+            if let error = error {
+                print("func mapView(_ mapView: MKMapView, didSelect... \n\(error)")
+                return
+            }
+            currentPin.photoCount = Int32(urls.count)
+            try? dataController.viewContext.save()
+            urls.forEach({ (currentURL) in
+                print("URL inside loop --> \(currentURL)")
+                URLSession.shared.dataTask(with: currentURL, completionHandler: { (imageData, response, error) in
+                    print("currentURL = \(currentURL)")
+                    guard let imageData = imageData else {return}
+                    self.connectPhotoAndPin(dataController: dataController, pin:  currentPin , data: imageData, urlString: currentURL.absoluteString)
+                }).resume()
+            })
+        }
+    }
+    
+    
+    
+    
+    
+    
 }
